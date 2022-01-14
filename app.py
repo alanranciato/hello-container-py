e4497768da3ad146935cdc1c3b3d92c75c4ce4e1
Alan Ranciato <alanranciato@gmail.com>
2022-01-14 13:33:39 -0500

    Update app.py
    New stuff

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from a GH Pull Request!!!!!!"
+    return "2 - Hello from a GH Pull Request!!!!!!"
 
 
 if __name__ == "__main__":
